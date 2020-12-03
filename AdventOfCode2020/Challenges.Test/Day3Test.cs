65aa67ca5b757272a2d923cb7379f0907f813178
byrongerlach <byrongerlach@users.noreply.github.com>
2020-12-03 10:12:33 -0800

    Day 3 challenges completed.

---
+++ AdventOfCode2020/Challenges.Test/Day3Test.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Challenges.Test
+{
+    /// <summary>
+    /// Tests for Day3
+    /// </summary>
+    [TestClass]
+    public class Day3Test
+    {
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        /// <summary>
+        /// Shows the CountTargetCharacters returns the correct target count.
+        /// </summary>
+        [TestMethod]
+        [DeploymentItem("InputFiles/Day3Problem1InputExample.txt")]
+        public void CountTargetCharacters_ValidInput_ExpectCorrectCount()
+        {
+            // ARRANGE 
+            var lines = File.ReadAllLines(@"Day3Problem1InputExample.txt");
+
+            // ACT 
+            var actual = Day3.CountTargetCharacters(lines, 3, 1, '#');
+
+            // ASSERT 
+            Assert.AreEqual(7, actual);
+
+        }
+
+        [TestMethod]
+        [DeploymentItem("InputFiles/Day3Problem1Input.txt")]
+        public void SolveProblem1()
+        {
+            // ARRANGE 
+            var lines = File.ReadAllLines(@"Day3Problem1Input.txt");
+
+            // ACT 
+            var actual = Day3.CountTargetCharacters(lines, 3, 1, '#');
+
+            // Output the result
+            TestContext.WriteLine($"Day 3 Problem 1: Found {actual} tree characters.");
+        }
+
+        [TestMethod]
+        [DeploymentItem("InputFiles/Day3Problem1Input.txt")]
+        public void SolveProblem2()
+        {
+            // ARRANGE 
+            var lines = File.ReadAllLines(@"Day3Problem1Input.txt");
+
+            // ACT 
+            var actual = Day3.CountTargetCharacters(lines, 1, 1, '#') *
+            Day3.CountTargetCharacters(lines, 3, 1, '#') *
+            Day3.CountTargetCharacters(lines, 5, 1, '#') *
+            Day3.CountTargetCharacters(lines, 7, 1, '#') *
+            Day3.CountTargetCharacters(lines, 1, 2, '#');
+
+            // Output the result
+            TestContext.WriteLine($"Day 3 Problem 2: Found {actual} tree characters.");
+        }
+
+    }
+}
---
+++ AdventOfCode2020/Challenges/Day3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class Day3
+    {
+        /// <summary>
+        /// Counts the target character encountered using the given right and down steps per iteration.
+        /// </summary>
+        /// <param name="lines">The lines.</param>
+        /// <param name="rightStep">The number of steps to go right in each iteration.</param>
+        /// <param name="downStep">The number of steps to go down in each iteration.</param>
+        /// <param name="targetChar">The target character.</param>
+        /// <returns>
+        /// Number of target characters
+        /// </returns>
+
+        public static int CountTargetCharacters(string[] lines, int rightStep, int downStep, char targetChar)
+        {
+            var x = rightStep;
+            var y = downStep;
+            var targetCount = 0;
+
+            var stepsTried = 0;
+
+            while (y < lines.Length) 
+            {
+                var currentLine = lines[y];
+                while (x >= currentLine.Length -1)
+                {
+                    currentLine += lines[y];
+                }
+
+                //if (lines[y][x] == targetChar)
+                if (currentLine[x] == targetChar)
+                {
+                    targetCount++;
+                }
+
+                x += rightStep;
+                y += downStep;
+                stepsTried++;
+            }
+
+            return targetCount;
+        }
+    }
+}
