e40cc5b6944f99c027e0841982b92e715b73e16b
byrongerlach <byrongerlach@users.noreply.github.com>
2020-12-04 16:27:25 -0800

    Problem 4 solution

---
+++ AdventOfCode2020/Challenges.Test/Day4Test.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Challenges.Test
+{
+    [TestClass]
+    public class Day4Test
+    {
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        [DeploymentItem("InputFiles/Day4Problem1InputExample.txt")]
+        public void GetPassports_InputValid_ExpectCorrectPassports()
+        {
+            // ARRANGE
+            var lines = File.ReadAllLines(@"Day4Problem1InputExample.txt");
+
+            // ACT
+            var actual = Day4.GetPassports(lines);
+
+            // ASSERT
+            Assert.AreEqual(4, actual.Count());
+        }
+
+        /// <summary>
+        /// Shows that we can get the passport fields from the 
+        /// data lines.
+        /// </summary>
+        /// <param name="lines"></param>
+        [TestMethod]
+        public void GetPassportFields_ValidInput_ExpectCorrectFields()
+        {
+            // ARRANGE
+             var lines = new[]
+             {
+                 @"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+                 @"byr:1937 iyr:2017 cid:147 hgt:183cm"
+             };
+
+            // ACT
+            var passport = new Day4.Passport() { DataLines = lines.ToList() };
+
+            passport.PopulateFields();
+
+            // ASSERT
+            Assert.AreEqual("gry", passport.PassportFields["ecl"]);
+            Assert.AreEqual("860033327", passport.PassportFields["pid"]);
+            Assert.AreEqual("2020", passport.PassportFields["eyr"]);
+            Assert.AreEqual( "#fffffd", passport.PassportFields["hcl"]);
+            Assert.AreEqual("1937", passport.PassportFields["byr"]);
+            Assert.AreEqual("2017", passport.PassportFields["iyr"]);
+            Assert.AreEqual("147", passport.PassportFields["cid"]);
+            Assert.AreEqual("183cm", passport.PassportFields["hgt"]);
+        }
+
+        /// <summary>
+        /// Shows that IsValid correctly computes passport rules.
+        /// </summary>
+        [TestMethod]
+        public void IsValid_DifferentInput_ExpectCorrectResult()
+        {
+
+            // ARRANGE
+            var passportFields = new Dictionary<string, string>
+            {
+                { "byr", "2002"},
+                { "iyr", "2010"},
+                { "eyr", "2020"},
+                { "hgt", "60in"},
+                { "hcl", "#123abc"},
+                { "ecl", "brn"},
+                { "pid", "000000001"},
+            };
+            
+            var passport = new Day4.Passport() { PassportFields = passportFields };
+
+            // ACT/ASSERT
+            Assert.IsTrue(passport.IsVaild());
+
+            //byr valid:   2002
+            //byr invalid: 2003
+
+            //hgt valid:   60in
+            //hgt valid:   190cm
+            //hgt invalid: 190in
+            //hgt invalid: 190
+
+            //hcl valid:   #123abc
+            //hcl invalid: #123abz
+            //hcl invalid: 123abc
+
+            //ecl valid: brn
+            //ecl invalid: wat
+
+            //pid valid: 000000001
+            //pid invalid: 0123456789
+        }
+
+        /// <summary>
+        /// Shows that we get the correct valid passports.
+        /// </summary>
+        [TestMethod]
+        public void GetValidPassports_ValidInput_ExpectCorrectPassports()
+        {
+            // ARRANGE
+            var lines = File.ReadAllLines(@"Day4Problem1InputExample.txt");
+
+            // ACT
+            var actual = Day4.GetPassports(lines).Where(passport => passport.IsVaild()).Count();
+
+            // ASSERT
+            Assert.AreEqual(2, actual);
+        }
+
+        /// <summary>
+        /// Solves problem 1 and outputs the answer.
+        /// </summary>
+        [TestMethod]
+        [DeploymentItem("InputFiles/Day4Problem1Input.txt")]
+        public void SolveProblem1()
+        {
+            // ARRANGE
+            var lines = File.ReadAllLines(@"Day4Problem1Input.txt");
+
+            // ACT
+            var actual = Day4.GetPassports(lines).Where(passport => passport.IsVaild()).Count();
+
+            // Output the result
+            TestContext.WriteLine($"Day 4 Problem 1: Found {actual} valid passports.");
+
+            // Output detail
+            var count = 1;
+            foreach (var passport in Day4.GetPassports(lines)) 
+            {
+                passport.PopulateFields();
+                var requiredFields = new HashSet<string> { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt" };
+
+                TestContext.WriteLine($"Passport #{count++}");
+
+                foreach (var field in requiredFields)
+                {
+                    if (passport.PassportFields.ContainsKey(field))
+                        TestContext.WriteLine($"{field}: {passport.PassportFields[field]} ");
+                    else      
+                        TestContext.WriteLine($"{field} not found");
+                }
+
+                TestContext.WriteLine($"{passport.IsVaild()}");
+
+                if (!passport.IsVaild())
+                {
+                    TestContext.WriteLine($"Invalid fields: {passport.InvalidFieldName} {passport.InvalidFieldValue}");
+                }
+
+                TestContext.WriteLine($"-----------");
+            }
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var number = "170cm";
+            var pattern = @"^([\d]+)(cm|in)";
+
+            var matches = Regex.Match(number, pattern);
+
+            Assert.IsTrue(matches.Success);
+
+            Assert.AreEqual("170", matches.Groups[1].Value);           
+            Assert.AreEqual("cm", matches.Groups[2].Value);           
+
+
+            matches = Regex.Match("cm", pattern);
+            Assert.IsFalse(matches.Success); 
+            
+            matches = Regex.Match("170", pattern);
+            Assert.IsFalse(matches.Success); 
+           
+
+        }
+    }
+}
---
+++ AdventOfCode2020/Challenges/Day4.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Challenges
+{
+    /// <summary>
+    /// Solution for Advent of Code Day 4
+    /// https://adventofcode.com/2020/day/4
+    /// </summary>
+    public class Day4
+    {
+        /// <summary>
+        /// Gets the passports. Each passport is separated by an empty blank line.
+        /// </summary>
+        /// <param name="lines">The lines.</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static IEnumerable<Passport> GetPassports(string[] lines)
+        {
+            var passport = new Passport();
+            foreach (var line in lines)
+            {
+                // Remove whitespace from the line
+                var currentLine = line.Trim();
+                // If the line's empty, we're at the end of the passport entry.
+                if (string.IsNullOrEmpty(currentLine))
+                {
+                    // Return the passport if it has any data lines.
+                    if (passport.DataLines.Any())
+                        yield return passport;
+
+                    // Start a new passport
+                    passport = new Passport();
+                }
+
+                // The line has content, so add it.
+                passport.DataLines.Add(line);
+            }
+
+            // Return the last passport
+            yield return passport;
+        }
+
+        public class Passport
+        {
+            /// <summary>
+            /// Gets the raw data lines for the passport.
+            /// </summary>
+            /// <value>
+            /// The data lines.
+            /// </value>
+            public List<string> DataLines { get; set; }
+
+            public Dictionary<string, string> PassportFields { get; set; }
+
+            /// <summary>
+            /// The name of the first invalid field found in the IsValid method
+            /// </summary>
+            public string InvalidFieldName { get; set; }
+ 
+            /// <summary>
+            /// The value of the first invalid field found in the IsValid method
+            /// </summary>
+            public string InvalidFieldValue { get; set; }
+           
+            public Passport()
+            {
+                DataLines = new List<string>();
+                PassportFields = new Dictionary<string, string>();
+            }
+
+            /// <summary>
+            /// Populates the passport fields from the DataLines.
+            /// </summary>
+            public void PopulateFields()
+            {
+                // Brute-force method of dealing with inconsistent line endings.
+                var data = string.Join("", DataLines.Select(d => d.Trim() + ' ').ToArray()).Trim();
+                var dataTokens = data.Split(' ');
+                foreach (var dataToken in dataTokens)
+                {
+                    var fieldTokens = dataToken.Split(':');
+
+                    // Skip if there's not two fields
+                    if (fieldTokens.Count() != 2)
+                        continue;
+
+                    PassportFields.Add(fieldTokens[0], fieldTokens[1]);
+                }
+            }
+
+            /// <summary>
+            /// Determines whether this passport is valid based on required fields.
+            /// </summary>
+            /// <returns>
+            ///   <c>true</c> if this instance is vaild; otherwise, <c>false</c>.
+            /// </returns>
+            public bool IsVaild()
+            {
+                /*
+                Here are the validation rules:
+                byr(Birth Year) - four digits; at least 1920 and at most 2002.
+                iyr(Issue Year) - four digits; at least 2010 and at most 2020.
+                eyr(Expiration Year) - four digits; at least 2020 and at most 2030.
+                hgt(Height) - a number followed by either cm or in:
+
+                    If cm, the number must be at least 150 and at most 193.
+                    If in, the number must be at least 59 and at most 76.
+
+                hcl(Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+                ecl(Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+                pid(Passport ID) - a nine - digit number, including leading zeroes.
+
+                */
+
+                var requiredFields = new HashSet<string> { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt" };
+
+                if (!PassportFields.Any())
+                    PopulateFields();
+
+                var suppliedFields = PassportFields.Select(f => f.Key).ToHashSet();
+
+                // Check that all required fields exist.
+                if (requiredFields.Except(suppliedFields).Any())
+                {
+                    return false;
+                }
+
+                // Check birth year.
+                var byr = Convert.ToInt32(PassportFields["byr"]);
+                if (byr < 1920 || byr > 2002)
+                {
+                    InvalidFieldName = "byr";
+                    InvalidFieldValue = byr.ToString();
+                    return false;
+                }
+
+                // Check issue year
+                var iyr = Convert.ToInt32(PassportFields["iyr"]);
+                if (iyr < 2010 || iyr > 2020)
+                {
+                    InvalidFieldName = "iyr";
+                    InvalidFieldValue = iyr.ToString();
+                    return false;
+                }
+
+                // Check expiration year
+                var eyr = Convert.ToInt32(PassportFields["eyr"]);
+                if (eyr < 2020 || eyr > 2030)
+                {
+                    InvalidFieldName = "eyr";
+                    InvalidFieldValue = eyr.ToString();
+                    return false;
+                }
+
+                // Check height
+                var matches = Regex.Match(PassportFields["hgt"], @"^([\d]+)(cm|in)");
+
+                if (!matches.Success)
+                {
+                    InvalidFieldName = "hgt";
+                    InvalidFieldValue = PassportFields["hgt"];
+                    return false;
+                }
+
+                var height = Convert.ToInt32(matches.Groups[1].Value);
+                var heightUnit = matches.Groups[2].Value;
+
+                if (heightUnit != "cm" && heightUnit != "in")
+                {
+                    InvalidFieldName = "hgt";
+                    InvalidFieldValue = PassportFields["hgt"];
+                    return false;
+                }
+
+                if (heightUnit == "cm" &&  (height < 150 || height > 193))
+                {
+                    InvalidFieldName = "hgt";
+                    InvalidFieldValue = PassportFields["hgt"];
+                    return false;
+                }
+
+                if (heightUnit == "in" &&  (height < 59 || height > 76))
+                {
+                    InvalidFieldName = "hgt";
+                    InvalidFieldValue = PassportFields["hgt"];
+                    return false;
+                }
+
+                // Check hair color
+                if (!Regex.IsMatch(PassportFields["hcl"], @"^#[0-9a-f]{6}"))
+                {
+                    InvalidFieldName = "hcl";
+                    InvalidFieldValue = PassportFields["hcl"];
+                    return false;
+                }
+
+                // Check eye color
+                var validColors = new HashSet<string> { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+
+                if (!validColors.Contains(PassportFields["ecl"]))
+                {
+                    InvalidFieldName = "ecl";
+                    InvalidFieldValue = PassportFields["ecl"];
+                    return false;
+                }
+
+                // Check passport ID
+                if (!Regex.IsMatch(PassportFields["pid"], @"^[0-9]{9}"))
+                {
+                    InvalidFieldName = "pid";
+                    InvalidFieldValue = PassportFields["pid"];
+                    return false;
+                }
+
+                return true;
+
+                /*
+                byr(Birth Year) - four digits; at least 1920 and at most 2002.
+                iyr(Issue Year) - four digits; at least 2010 and at most 2020.
+                eyr(Expiration Year) - four digits; at least 2020 and at most 2030.
+                hgt(Height) - a number followed by either cm or in:
+
+                    If cm, the number must be at least 150 and at most 193.
+                    If in, the number must be at least 59 and at most 76.
+
+                hcl(Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+                ecl(Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+                pid(Passport ID) - a nine - digit number, including leading zeroes.
+
+                */
+            }
+        }
+    }
+}
